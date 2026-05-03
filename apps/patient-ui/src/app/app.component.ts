aed38b4ddd11c21a95c34a49e7a6248602364968
Tom L'Hotellier <119570582+TomitoBrees@users.noreply.github.com>
2026-05-03 12:39:18 +0200

    Moved headerbar to layout

--- apps/patient-ui/src/app/app.component.ts
+++ apps/patient-ui/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { HeaderBar } from "./layout/header-bar/header-bar.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss"
+  styleUrl: "./app.component.scss",
+  imports: [HeaderBar]
 })
 export class AppComponent {}
